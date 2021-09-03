3646733f6e25ac2c4a25714b2ce76c984f21e3a1
SlaypZ <gustavoasf77@gmail.com>
2021-09-03 20:07:02 -0300

    created Contatos

---
+++ Contatos/app/src/main/java/com/example/contatos/ClickItemContactListener.kt
@@ -0,0 +1,5 @@
+package com.example.contatos
+
+interface ClickItemContactListener {
+    fun clickItemContact(contact: Contact)
+}
---
+++ Contatos/app/src/main/java/com/example/contatos/ContactDetail.kt
@@ -0,0 +1,42 @@
+package com.example.contatos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+
+class ContactDetail : AppCompatActivity() {
+    private var contact: Contact? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_detail)
+        getExtras()
+        bindView()
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun getExtras() {
+        contact = intent.getParcelableExtra(EXTRA_CONTACT)
+    }
+
+    private fun bindView() {
+        findViewById<TextView>(R.id.tv_name).text = contact?.name
+        findViewById<TextView>(R.id.tv_phone).text = contact?.phone
+    }
+
+
+    companion object{
+        const val EXTRA_CONTACT: String = "EXTRA_CONTACT"
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return super.onSupportNavigateUp()
+    }
+}
